a93f6003fe986752adc7a64e42f2e1aad37eabcc
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2023-10-18 14:10:21 -0700

    Update myApiOneHandler.go

--- grpcTemplate/internal/routes/myApiOneHandler.go
+++ grpcTemplate/internal/routes/myApiOneHandler.go
@@ -4,18 +4,41 @@ import (
 	"context"
 	"google.golang.org/genproto/googleapis/api/httpbody"
 	"google.golang.org/protobuf/types/known/emptypb"
+	pb "grpcTemplate/pkg"
 )
 
-type MyFirstApi struct {
+type MyApiOneServer struct {
+	pb.UnimplementedMyFirstApiServer
 }
 
-func (*MyFirstApi) FirstGetRpc(ctx context.Context, request *httpbody.HttpBody, in *emptypb.Empty) (*httpbody.HttpBody, error) {
+func (*MyApiOneServer) FirstGetRpc(ctx context.Context, in *emptypb.Empty) (*httpbody.HttpBody, error) {
 	result := "This is a hardcoded response."
+	responseData := "Response: " + result
+
+	pretty := ctx.Value("pretty")
+	if pretty != nil {
+		responseData = "Pretty Response: " + result
+	}
 
 	response := &httpbody.HttpBody{
 		ContentType: "text/html",
-		Data:        []byte("Response: " + result),
+		Data:        []byte(responseData),
+	}
+	return response, nil
+}
+
+func (*MyApiOneServer) SecondGetRpc(ctx context.Context, in *emptypb.Empty) (*httpbody.HttpBody, error) {
+	result := "This is a hardcoded response."
+	responseData := "Response: " + result
+
+	pretty := ctx.Value("pretty")
+	if pretty != nil {
+		responseData = "Pretty Response: " + result
 	}
 
+	response := &httpbody.HttpBody{
+		ContentType: "text/html",
+		Data:        []byte(responseData),
+	}
 	return response, nil
 }
